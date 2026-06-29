49511c0fd5e093efbdaab5e7431ba1bf97f0fe16
Idraezy <idraezynoks@gmail.com>
2026-06-29 12:33:48 +0100

    Add project images, adjust CSS import, rename Tech
    Add 11 new images to the project13 gallery (project13d.png through project13n.png) to expand the project media set. In app/layout.tsx, add a // @ts-ignore before importing globals.css (and leave a commented import) to suppress a TypeScript import warning. In app/sections/About.tsx, rename the subsection heading from "Tech" to "Tech Stack" for clarity.

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
+// import "./globals.css";
+// @ts-ignore
 import "./globals.css";
 import Navbar from "@/components/Navbar";
 import BackgroundAnimation from "@/components/BackgroundAnimation";
